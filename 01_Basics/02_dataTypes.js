2968808e148ffbdef38d0ee65647a2f0bf3e3673
shivam898989 <135410610+shivam898989@users.noreply.github.com>
2024-01-08 04:34:07 +0000

    Datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3) // we are using node js not broweser
+
+console.log(3
+    +3); // code readability should be high
+
+console.log("Shhhivam");
+
+// number => 2 to power 53
+// bigint
+// srting = ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+// symbol => unique
+
+
+//object
+console.log(typeof null) // object
+console.log(typeof undefined) //undefined
