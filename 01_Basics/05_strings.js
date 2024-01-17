999737fe75e8d804134ab526e9555eccabe886f0
shivam898989 <135410610+shivam898989@users.noreply.github.com>
2024-01-17 20:46:17 +0000

    strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,14 @@
+const name = "shivam"
+const repoCount = 50
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("shivam")
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf("a"));
