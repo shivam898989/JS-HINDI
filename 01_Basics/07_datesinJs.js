a71e8833e6f4ef75a4afac07167b3be72aedd41b
shivam898989 <135410610+shivam898989@users.noreply.github.com>
2024-01-19 23:49:54 +0000

    DATE

---
+++ 01_Basics/07_datesinJs.js
@@ -0,0 +1,16 @@
+// dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleString());
+
+let myCreatedDate = new Date("01-14-2024")
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
