c860a272dcd85250a9da80c6c459e857efbaab6b
shivam898989 <135410610+shivam898989@users.noreply.github.com>
2024-01-20 02:06:04 +0000

    date

--- 01_Basics/07_datesinJs.js
+++ 01_Basics/07_datesinJs.js
@@ -12,10 +12,16 @@ let myCreatedDate = new Date("01-14-2024")
 
 let myTimeStamp = Date.now()
 
-console.log(myTimeStamp);
-console.log(myCreatedDate.getTime());
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
 
 let newDate = new Date()
-console.log(newDate);
-console.log(newDate.getMonth()+1);
-console.log(newDate.getDay());
+// console.log(newDate);
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getDay());
+
+//`${newDate.getDay()} and the time`
+
+newDate.toLocaleDateString('default', {
+    weekday: "long",
+}) 
